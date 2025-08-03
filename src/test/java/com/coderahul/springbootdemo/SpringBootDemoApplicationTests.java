5ddc96ee992dbd61a855b06f884d893b087e836d
Rahul Anand <rahulanand90@gmail.com>
2025-08-03 08:06:25 +0530

    fix: Remove DB Dependency from Test

--- src/test/java/com/coderahul/springbootdemo/SpringBootDemoApplicationTests.java
+++ src/test/java/com/coderahul/springbootdemo/SpringBootDemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.coderahul.springbootdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootDemoApplicationTests {
 
 	@Test
